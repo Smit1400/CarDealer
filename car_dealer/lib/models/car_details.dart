22c37e5103c231bdaf552a44fc656d4581f008cc
Smit1400 <46996928+Smit1400@users.noreply.github.com>
2021-02-15 17:05:45 +0530

    car details added

---
+++ car_dealer/lib/models/car_details.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/foundation.dart';
+
+class CarDetails {
+  final String carId;
+  final String userId;
+  final String ownerName;
+  final int mobileNumber;
+  final String brand;
+  final double mileage;
+  final double kilometer_driven;
+  final String owner_type;
+  final double engine;
+  final int seats;
+  final double power;
+  final int year;
+  final double price;
+  final String fuel_type;
+  final String title;
+  final String description;
+
+  CarDetails({
+    @required this.brand,
+    @required this.carId,
+    @required this.userId,
+    @required this.ownerName,
+    @required this.mileage,
+    @required this.kilometer_driven,
+    @required this.engine,
+    @required this.owner_type,
+    @required this.power,
+    @required this.price,
+    @required this.seats,
+    @required this.year,
+    @required this.fuel_type,
+    @required this.title,
+    @required this.description,
+    @required this.mobileNumber,
+  });
+
+  factory CarDetails.fromMap(Map<String, dynamic> data) {
+    return CarDetails(
+      brand: data["brand"],
+      engine: data["engine"],
+      mileage: data["mileage"],
+      kilometer_driven: data["kilometer_driven"],
+      power: data["power"],
+      price: data["price"],
+      seats: data["seats"],
+      owner_type: data["owner_type"],
+      year: data["year"],
+      carId: data["carId"],
+      userId: data["userId"],
+      ownerName: data["ownerName"],
+      fuel_type: data["fuel_type"],
+      description: data["description"],
+      title: data["title"],
+      mobileNumber: data["mobileNumber"],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "brand": this.brand,
+      "engine": this.engine,
+      "mileage": this.mileage,
+      "kilometer_driven": this.kilometer_driven,
+      "power": this.power,
+      "price": this.price,
+      "seats": this.seats,
+      "owner_type": this.owner_type,
+      "year": this.year,
+      "carId": this.carId,
+      "userId": this.userId,
+      "ownerName": this.ownerName,
+      "fuel_type": this.fuel_type,
+      "title": this.title,
+      "description": this.description,
+      "mobileNumber":  this.mobileNumber,
+    };
+  }
+}
