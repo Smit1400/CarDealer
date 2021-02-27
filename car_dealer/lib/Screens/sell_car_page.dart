b3d2d4170c4d0209ce313969dbcf9fb0667167fa
Smit1400 <46996928+Smit1400@users.noreply.github.com>
2021-02-27 09:25:41 +0530

    multiple_image_picker integrated

--- car_dealer/lib/Screens/sell_car_page.dart
+++ car_dealer/lib/Screens/sell_car_page.dart
@@ -7,8 +7,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 import 'package:path/path.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'dart:io';
 
@@ -192,11 +194,16 @@ class _SellCarState extends State<SellCar> {
     'Ferrari'
   ];
 
+  List<Asset> images = List<Asset>();
+  List<File> files = List<File>();
+
   String _selectedbrand, _selectedfuel, _selectedowner, _title, _description;
   int _year, _km, _price, _mileage, _engine, _seats, _power;
 
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
+
+  
 
   List<String> _fueltypes = [
     'CNG & hybrids',
@@ -210,6 +217,47 @@ class _SellCarState extends State<SellCar> {
 
   Map arguments = {};
 
+  Future<void> loadAssets() async {
+    List<Asset> resultList = List<Asset>();
+    String error = 'No Error Dectected';
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+        maxImages: 300,
+        enableCamera: true,
+        selectedAssets: images,
+        cupertinoOptions: CupertinoOptions(takePhotoIcon: "chat"),
+        materialOptions: MaterialOptions(
+          actionBarColor: "#ba5fe8",
+          actionBarTitle: "Car Dealer",
+          allViewTitle: "All Photos",
+          useDetailsView: false,
+          selectCircleStrokeColor: "#000000",
+        ),
+      );
+    } on Exception catch (e) {
+      error = e.toString();
+      print("[ERROR] $error");
+    }
+
+    if (!mounted) return;
+
+    for (int i = 0; i < resultList.length; i++) {
+      var byteData = await resultList[i].getByteData();
+
+      var tempFile =
+          File("${(await getTemporaryDirectory()).path}/${resultList[i].name}");
+      var file = await tempFile.writeAsBytes(
+        byteData.buffer
+            .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes),
+      );
+
+      setState(() {
+        files.add(file);
+      });
+    }
+  }
+
   _uploadImage() async {
     var tempImage = await ImagePicker().getImage(source: ImageSource.gallery);
     setState(() {
@@ -217,16 +265,21 @@ class _SellCarState extends State<SellCar> {
     });
   }
 
-  Future<String> _getDownLoadUrl(BuildContext context) async {
+  Future<List<dynamic>> _getDownLoadUrl(BuildContext context) async {
     try {
-      final Reference firebaseStorageRef = FirebaseStorage.instance
-          .ref()
-          .child('car_images/${basename(_selectedImage.path)}');
-      final TaskSnapshot task =
-          await firebaseStorageRef.putFile(_selectedImage);
-      print("[INFO] Successfully stored image in firebase storage.");
-      String url = await task.ref.getDownloadURL();
-      return url;
+      List urls = [];
+      for (int i = 0; i < files.length; i++) {
+        final Reference firebaseStorageRef = FirebaseStorage.instance
+            .ref()
+            .child('car_images/${basename(files[i].path)}');
+        final TaskSnapshot task = await firebaseStorageRef.putFile(files[i]);
+        print("[INFO] Successfully stored image in firebase storage.");
+        String url = await task.ref.getDownloadURL();
+        setState(() {
+          urls.add(url);
+        });
+      }
+      return urls;
     } catch (e) {
       throw e.toString();
     }
@@ -245,15 +298,15 @@ class _SellCarState extends State<SellCar> {
       setState(() {
         _loading = true;
       });
-      if (_selectedImage != null) {
-        String imageUrl = await _getDownLoadUrl(context);
-        List imageUrls = [imageUrl];
+      if (files != null && files.length > 0) {
+        List imageUrls = await _getDownLoadUrl(context);
+        // List imageUrls = [imageUrl];
         String carId = "cars_${DateTime.now().toIso8601String()}";
         CarDetails carDetails = CarDetails(
             brand: _selectedbrand,
             carId: carId,
             userId: _firebaseServices.getUserId(),
-            ownerName: arguments["username"],
+            ownerName: widget.username,
             mileage: _mileage.toDouble(),
             kilometer_driven: _km.toDouble(),
             engine: _engine.toDouble(),
@@ -274,11 +327,6 @@ class _SellCarState extends State<SellCar> {
             gravity: Toast.TOP,
             textColor: Colors.white,
             backgroundColor: Colors.black.withOpacity(0.6));
-
-        var count = 0;
-        Navigator.popUntil(context, (route) {
-          return count++ == 2;
-        });
       } else {
         _scaffoldKey.currentState
             .showSnackBar(SnackBar(content: Text("No Image was selected")));
@@ -301,7 +349,6 @@ class _SellCarState extends State<SellCar> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
         key: _scaffoldKey,
-        appBar: AppBar(title: Text("Car Dealer App")),
         resizeToAvoidBottomPadding: false,
         // drawer: MySideBar(),
         body: Stack(children: [
@@ -312,13 +359,24 @@ class _SellCarState extends State<SellCar> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(top: 15),
+                      width: size.width,
+                      child: Center(
+                        child: Text("Enter Car Details",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold)),
+                      ),
+                    ),
                     Form(
                       key: _formKey,
                       autovalidateMode: AutovalidateMode.disabled,
                       child: Column(
                         children: <Widget>[
                           Padding(
-                            padding: EdgeInsets.only(top: 50),
+                            padding: EdgeInsets.only(top: 25),
                           ),
                           Container(
                             decoration: BoxDecoration(
@@ -514,7 +572,7 @@ class _SellCarState extends State<SellCar> {
                           ),
                           imageUploadWidget(),
                           SubmitBtn(
-                            text: "Sell Car",
+                            text: "Register Car",
                             onPressed: () => _submit(context),
                             sizeW: size.width,
                           )
@@ -526,12 +584,6 @@ class _SellCarState extends State<SellCar> {
               ),
             ),
           ),
-          CustomActionBar(
-              hasBackArrrow: true,
-              title: "Sell car",
-              hasTitle: true,
-              hasBackground: false,
-              hasCount: false),
           _loading == true
               ? Container(
                   color: Colors.black.withOpacity(0.5),
@@ -565,8 +617,8 @@ class _SellCarState extends State<SellCar> {
               ),
             ],
           ),
-          child: (_selectedImage != null)
-              ? Image.file(_selectedImage)
+          child: (files.length > 0 && files != null)
+              ? Image.file(files[0])
               : Image.network('https://i.imgur.com/sUFH1Aq.png')),
       SizedBox(
         height: 20.0,
@@ -578,7 +630,7 @@ class _SellCarState extends State<SellCar> {
                 fontWeight: FontWeight.bold,
                 fontSize: 20)),
         onPressed: () {
-          _uploadImage();
+          loadAssets();
         },
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(18.0),
